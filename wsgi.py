f31d61341de13f8999313e4b00dd7e26aafdb33b
barry9090 <bilalarshadtts@gmail.com>
2025-08-22 12:06:02 +0500

    db error

--- wsgi.py
+++ wsgi.py
@@ -7,9 +7,11 @@ import os
 
 app = create_app()
 
+# Create tables on startup (for production)
+with app.app_context():
+    db.create_all()
+    print("Database tables initialized")
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
